0767b74911455706eeff8f64908e495e9dafc49d
victoriasyluma <victoriasyluma3@gmail.com>
2023-05-28 23:47:49 +0200

    adding jest for unit test

---
+++ jest.setup.ts
@@ -0,0 +1,5 @@
+beforeEach(() => {
+  jest.resetAllMocks();
+  jest.clearAllMocks();
+  jest.clearAllTimers();
+});
---
+++ src/routes/Users/Users.test.tsx
@@ -0,0 +1,11 @@
+// import React from 'react';
+// import { render, fireEvent } from '@testing-library/react';
+
+describe('Counter component', () => {
+  it('renders initial count', () => {
+    // const { getByText } = render(<Counter />);
+    // const countElement = getByText(/Count:/i);
+
+    expect(true).toBe(true);
+  });
+});
